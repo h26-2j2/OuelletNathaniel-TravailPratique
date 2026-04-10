61c0b1a3144d269cd88ae01f23c793f967da2264
Ouellet-Nathaniel <e2555551@cmaisonneuve.qc.ca>
2026-04-09 20:34:19 -0400

    Scene change implemented

--- Assets/Scripts/Fader.cs
+++ Assets/Scripts/Fader.cs
@@ -1,9 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fader : MonoBehaviour
 {
+    [SerializeField] Image target;
+    [SerializeField] float duration = 1f;
+    [SerializeField] AnimationCurve fade;
+    [SerializeField] bool fadeOnAwake;
+    bool fading;
+    float time;
+
+    void Awake()
+    {
+        if (target == null)
+        {
+            target = gameObject.GetComponent<Image>();
+        }
+
+        if (fadeOnAwake)
+        {
+            time = 0f;
+            StartFading();
+        }
+    }
+
     void Update()
     {
-        
+        if (fading)
+        {
+            {
+                time += Time.deltaTime / duration;
+                time = Mathf.Clamp(time, 0f, 1f);
+            }
+            target.color = new Color(target.color.r, target.color.g, target.color.b, fade.Evaluate(time));
+        }
+    }
+
+    public void StartFading()
+    {
+        fading = true;
     }
 }
